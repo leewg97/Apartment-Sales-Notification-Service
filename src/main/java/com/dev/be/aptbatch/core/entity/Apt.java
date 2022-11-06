8e12fb72645e28869f5df3825dfb83816acbafca
leewg97 <leewg97@naver.com>
2022-11-07 02:24:43 +0900

    Add #1 - Apt Entity * 아파트 테이블

---
+++ src/main/java/com/dev/be/aptbatch/core/entity/Apt.java
@@ -0,0 +1,43 @@
+package com.dev.be.aptbatch.core.entity;
+
+import lombok.Data;
+import org.hibernate.annotations.DynamicUpdate;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@DynamicUpdate
+@Table(name = "apt")
+@EntityListeners(AuditingEntityListener.class)
+public class Apt {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long aptId;
+
+    @Column(nullable = false)
+    private String aptName;
+
+    @Column(nullable = false)
+    private String jibun;
+
+    @Column(nullable = false)
+    private String dong;
+
+    @Column(nullable = false)
+    private String guLawdCd;
+
+    @Column(nullable = false)
+    private Integer builtYear;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+}
