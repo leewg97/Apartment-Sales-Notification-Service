b4eec1f1392d6caa6161a7cafc5e55c77fd77b74
leewg97 <leewg97@naver.com>
2022-11-07 01:13:02 +0900

    gradle init

---
+++ src/main/java/com/dev/be/aptbatch/AptBatchApplication.java
@@ -0,0 +1,13 @@
+package com.dev.be.aptbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AptBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AptBatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dev/be/aptbatch/AptBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.be.aptbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AptBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
