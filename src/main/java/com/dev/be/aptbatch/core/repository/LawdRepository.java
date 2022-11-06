d64421acc48bf7d378af2d4af6629354fa378941
leewg97 <leewg97@naver.com>
2022-11-07 02:17:58 +0900

    Add #1 - LawdRepository

---
+++ src/main/java/com/dev/be/aptbatch/core/repository/LawdRepository.java
@@ -0,0 +1,7 @@
+package com.dev.be.aptbatch.core.repository;
+
+import com.dev.be.aptbatch.core.entity.Lawd;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LawdRepository extends JpaRepository<Lawd, Long> {
+}
