de70cecaadf699c8fffae6870ca8e04468014617
leewg97 <leewg97@naver.com>
2022-11-07 10:18:42 +0900

    Update #3 - AptDealService

--- src/main/java/com/dev/be/aptbatch/core/service/AptDealService.java
+++ src/main/java/com/dev/be/aptbatch/core/service/AptDealService.java
@@ -1,6 +1,7 @@
 package com.dev.be.aptbatch.core.service;
 
 import com.dev.be.aptbatch.core.dto.AptDealDto;
+import com.dev.be.aptbatch.core.dto.AptDto;
 import com.dev.be.aptbatch.core.entity.Apt;
 import com.dev.be.aptbatch.core.entity.AptDeal;
 import com.dev.be.aptbatch.core.repository.AptDealRepository;
@@ -8,6 +9,10 @@ import com.dev.be.aptbatch.core.repository.AptRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * AptDealDto 에 있는 값을 Apt, AptDeal 엔티티로 저장
@@ -33,12 +38,24 @@ public class AptDealService {
 
     private void saveAptDeal(AptDealDto dto, Apt apt) {
         AptDeal aptDeal = aptDealRepository.findAptDealByAptAndExclusiveAreaAndDealDateAndDealAmountAndFloor(
-                        apt, dto.getExclusiveArea(),dto.getDealDate(), dto.getDealAmount(), dto.getFloor())
-                .orElseGet(() -> AptDeal.of(dto, apt));
+                apt,
+                dto.getExclusiveArea(),
+                dto.getDealDate(),
+                dto.getDealAmount(),
+                dto.getFloor()
+        ).orElseGet(() -> AptDeal.of(dto, apt));
+
         aptDeal.setDealCanceled(dto.isDealCanceled());
         aptDeal.setDealCanceledDate(dto.getDealCanceledDate());
         aptDealRepository.save(aptDeal);
     }
 
+    public List<AptDto> findByGuLawdCdAndDealDate(String guLawdCd, LocalDate dealDate) {
+        return aptDealRepository.findByDealCanceledIsFalseAndDealDateEquals(dealDate)
+                    .stream()
+                    .filter(aptDeal -> aptDeal.getApt().getGuLawdCd().equals(guLawdCd))
+                    .map(aptDeal -> new AptDto(aptDeal.getApt().getAptName(), aptDeal.getDealAmount()))
+                    .collect(Collectors.toList());
+    }
 
 }
